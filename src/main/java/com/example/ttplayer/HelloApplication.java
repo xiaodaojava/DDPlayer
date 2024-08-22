553b320f18174c0b6c7375a2cc9abea2a5361616
xiaodaojava <good396975802@qq.com>
2024-08-22 23:04:13 +0800

    修改了启动时的大小和版本号

--- src/main/java/com/example/ttplayer/HelloApplication.java
+++ src/main/java/com/example/ttplayer/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(),400,300);
         stage.setScene(scene);
         stage.setTitle("DDPlayer-支持多个视频同时播放");
         stage.show();
